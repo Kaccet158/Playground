ab2dab84bb1ded77a8593abe2242a432c833d66e
Kacper <kacperceteraonett@gmail.com>
2026-02-23 10:30:50 +0100

    Improve program's visibility

--- simple-notepad/simple-notepad.py
+++ simple-notepad/simple-notepad.py
@@ -4,8 +4,12 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center;'>Simple Notepad</h1>",unsafe_allow_html=True)
 
 cols = st.columns([2,2])
-with cols[0]: code = st.text_input("Code")
-with cols[1]: runCode = st.button("Run", use_container_width = True)
+with cols[0]: 
+    st.markdown("**Code**")
+    code = st.text_input("",label_visibility="collapsed") #Hide label
+with cols[1]: 
+    st.markdown("**Start**")
+    runCode = st.button("Run", use_container_width = True)
 
 if runCode:
     result = code.lower()
