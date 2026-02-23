3e8c8e8c2685f829620c50bb5130d3a40afe1869
Kacper <kacperceteraonett@gmail.com>
2026-02-23 10:47:35 +0100

    Move texts to the middle

--- simple-notepad/simple-notepad.py
+++ simple-notepad/simple-notepad.py
@@ -5,10 +5,10 @@ st.markdown("<h1 style='text-align: center;'>Simple Notepad</h1>",unsafe_allow_h
 
 cols = st.columns([2,2])
 with cols[0]: 
-    st.markdown("**Code**")
+    st.markdown("<p style='text-align: center;'><b>Code</b></p>",unsafe_allow_html=True)
     code = st.text_input("",label_visibility="collapsed") #Hide label
 with cols[1]: 
-    st.markdown("**Start**")
+    st.markdown("<p style='text-align: center;'><b>Start</b></p>",unsafe_allow_html=True)
     runCode = st.button("Run", use_container_width = True)
 
 if runCode:
